acf7f5750998ef2f50b315dbbccb31b0634f7882
ek47120 <ek47120@ubt-uni.net>
2022-01-17 14:33:57 +0100

    style-end of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
-
+import 'react-calendar/dist/Calendar.css'
 import reportWebVitals from './reportWebVitals';
 import App from './app/layout/App';
 import { store, StoreContext } from './app/stores/store';
