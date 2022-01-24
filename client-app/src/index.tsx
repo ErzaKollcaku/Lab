0323a01f785e5ef446b4c5f5e5abaed67d95463c
ek47120 <ek47120@ubt-uni.net>
2022-01-24 22:20:28 +0100

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import 'react-calendar/dist/Calendar.css'
 import reportWebVitals from './reportWebVitals';
 import App from './app/layout/App';
